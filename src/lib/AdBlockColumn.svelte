385ae455f1ef7fff89eb11e3205a247fb7df9fe4
Jeffrey Hugh <j@jh.ms>
2025-05-02 23:07:26 +0000

    fix: make ads more easily targetable

--- src/lib/AdBlockColumn.svelte
+++ src/lib/AdBlockColumn.svelte
@@ -2,7 +2,11 @@
 	const { children } = $props();
 </script>
 
-<div class="mb-8 hidden max-w-1/4 grow overflow-auto px-12 lg:block">
+<div
+	id="ad"
+	class="advertisement sponsor mb-8 hidden max-w-1/4 grow overflow-auto px-12 lg:block"
+	data-ad="true"
+>
 	<div class="text-base-content/80 pl-1 text-xs lowercase">Advertisement</div>
 	<div class="border-base-300 overflow-hidden border">
 		{@render children()}
--- src/lib/AdBlockSmall.svelte
+++ src/lib/AdBlockSmall.svelte
@@ -2,7 +2,7 @@
 	const { children } = $props();
 </script>
 
-<div class="mt-2 w-full lg:hidden">
+<div id="ad" class="advertisement sponsor mt-2 w-full lg:hidden" data-ad="true">
 	<div class="text-base-content/80 ml-1 text-xs lowercase">Advertisement</div>
 	<div class="border-base-300 overflow-hidden border">
 		{@render children()}
