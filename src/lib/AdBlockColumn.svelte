6227458696c967567f3a38b65a8a7d2739bf69b7
Jeffrey Hugh <j@jh.ms>
2025-05-20 09:27:30 +0000

    feat: add env to hide ads

--- src/lib/AdBlockColumn.svelte
+++ src/lib/AdBlockColumn.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+	import { PUBLIC_NOADS } from '$env/static/public';
 	const { children } = $props();
 </script>
 
-<div
-	id="ad"
-	class="advertisement sponsor mb-8 hidden max-w-1/4 grow overflow-auto px-12 lg:block"
-	data-ad="true"
->
-	<div class="text-base-content/80 pl-1 text-xs lowercase">Advertisement</div>
-	<div class="border-base-300 overflow-hidden border">
-		{@render children()}
+{#if !PUBLIC_NOADS}
+	<div
+		id="ad"
+		class="advertisement sponsor mb-8 hidden max-w-1/4 grow overflow-auto px-12 lg:block"
+		data-ad="true"
+	>
+		<div class="text-base-content/80 pl-1 text-xs lowercase">Advertisement</div>
+		<div class="border-base-300 overflow-hidden border">
+			{@render children()}
+		</div>
 	</div>
-</div>
+{/if}
