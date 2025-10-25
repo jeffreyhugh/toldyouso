03c8bdf84ddb3a8859d15413519c3d5cb623aca2
Jeffrey Hugh <j@jh.ms>
2025-10-25 00:50:15 +0000

    revert: remove divs around ins

--- src/lib/AdBeforeSubmit.svelte
+++ src/lib/AdBeforeSubmit.svelte
@@ -12,13 +12,11 @@
 	});
 </script>
 
-<div>
-	<ins
-		class="adsbygoogle"
-		style="display:block"
-		data-ad-client="ca-pub-7806885462809506"
-		data-ad-slot={BeforeSubmit}
-		data-ad-format="auto"
-		data-full-width-responsive="true"
-	></ins>
-</div>
+<ins
+	class="adsbygoogle"
+	style="display:block"
+	data-ad-client="ca-pub-7806885462809506"
+	data-ad-slot={BeforeSubmit}
+	data-ad-format="auto"
+	data-full-width-responsive="true"
+></ins>
